628d184a3961cd8df1edac98bfa97c587f9a965b
NETbrew <r0929448@student.thomasmore.be>
2024-10-09 15:13:41 +0200

    dockerfile

--- labflow-backend/src/test/java/com/thomasmore/blc/labflow/LabflowApplicationTests.java
+++ labflow-backend/src/test/java/com/thomasmore/blc/labflow/LabflowApplicationTests.java
@@ -1,5 +1,6 @@
 package com.thomasmore.blc.labflow;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class LabflowApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void helloWorld() {
+		String x = "Jan met de pet";
+		Assertions.assertEquals("Jan met de pet", x);
 	}
 
 }
