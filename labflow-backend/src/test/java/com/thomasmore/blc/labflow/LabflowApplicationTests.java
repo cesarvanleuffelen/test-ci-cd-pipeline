08256ec09f518778e2b3386eb1f8b55855cfd23b
NETbrew <r0929448@student.thomasmore.be>
2024-10-09 16:09:12 +0200

    updated a test

--- labflow-backend/src/test/java/com/thomasmore/blc/labflow/LabflowApplicationTests.java
+++ labflow-backend/src/test/java/com/thomasmore/blc/labflow/LabflowApplicationTests.java
@@ -9,8 +9,7 @@ class LabflowApplicationTests {
 
 	@Test
 	void helloWorld() {
-		String x = "Jan met de pet";
-		Assertions.assertEquals("Jan met de pet", x);
+		Assertions.assertTrue(true);
 	}
 
 }
